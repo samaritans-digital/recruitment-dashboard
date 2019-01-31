bc5c46bd23e854728aa22d7ec99025ef8b7f79bb
Joshua Hackett <joshua.hackett93@gmail.com>
2019-01-31 16:56:19 +0000

    uses email template from mandrill now

--- utils/emails.js
+++ utils/emails.js
@@ -3,26 +3,30 @@ const mandrillClient = new mandrill.Mandrill(process.env.MANDRILL_API_KEY)
 
 const sendEmail = (tokenToSend, uidToSend, recipient) => {
     const url = `${process.env.HOST}/login/finish?token=${tokenToSend}&uid=${encodeURIComponent(uidToSend)}`
+   
     const message = {
-        html: `
-            <p>Click the link to finish logging in:</p>
-            <a href="${url}">Finish logging in</a>
-            <p>If you didn't request this link, contact the Samaritans helpdesk immediately.</p>
-        `,
-        text: `Go to this URL to finish logging in: ${url}`,
-        subject: "Finish logging into the recruitment dashboard",
         from_email: "donotreply@samaritans-volunteers.org",
         from_name: "Samaritans",
-        to: [{"email": recipient}]
+        to: [{"email": recipient}],
+        global_merge_vars: [{
+            name: "recdashboard_url",
+            content: url
+        }],
+        track_clicks: false
     }
-    mandrillClient.messages.send({"message": message}, 
-        result => {
-            // On success
-            console.log(result)
-        }, e => {
-            // On error
-            console.log("An error occurred", e)
-        })
+
+    mandrillClient.messages.sendTemplate({
+        template_name: "dashboard-finish-logging-in",
+        template_content: {},
+        message: message
+    }, 
+    result => {
+        // On success
+        console.log(result)
+    }, e => {
+        // On error
+        console.log("An error occurred", e)
+    })
 }
 
 module.exports = {
